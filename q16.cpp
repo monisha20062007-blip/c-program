eb21c317b949600751e004744b92b1000b1874a2
monisha20062007-blip <monisha20062007@gmail.com>
2025-09-24 14:59:10 +0530

    Add files via upload

---
+++ q16.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+      if ((num % 2 == 0) && (num % 5 == 0) && (num % 8 != 0)) {
+        printf("%d is divisible by both 2 and 5, but not by 8.\n", num);
+    } else {
+        printf("%d \n", num);
+    }
+
+    return 0;
+}
